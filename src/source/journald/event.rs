02b8b24878a2e234961156dcbba0aa71ed72f56b
Max Eliseev <ralvke@gmail.com>
2019-02-20 21:35:46 +0500

    Crop ansi codes (colors) from journald source

--- src/source/journald/event.rs
+++ src/source/journald/event.rs
@@ -1,6 +1,16 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use systemd::journal::JournalRecord;
 
 use crate::source::LogRecord;
+
+fn crop_ansi_codes(input: String) -> String {
+    lazy_static! {
+        static ref RE: Regex = Regex::new("\x1b\\[[^@-~]*[@-~]").unwrap();
+    };
+
+    RE.replace_all(&input, "").to_string()
+}
 
 #[derive(Debug)]
 pub struct JournaldEvent {
@@ -11,7 +21,11 @@ pub struct JournaldEvent {
 impl From<JournalRecord> for JournaldEvent {
     fn from(record: JournalRecord) -> Self {
         let unit = record.get("_SYSTEMD_UNIT").cloned().unwrap_or_default();
-        let message = record.get("MESSAGE").cloned().unwrap_or_default();
+        let message = record
+            .get("MESSAGE")
+            .cloned()
+            .map(crop_ansi_codes)
+            .unwrap_or_default();
 
         JournaldEvent { unit, message }
     }
