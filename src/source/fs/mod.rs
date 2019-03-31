f9cff4070ccef22b8af3ed58614640e9596a2f46
Max Eliseev <ralvke@gmail.com>
2019-03-31 22:39:17 +0500

    Reorganize tests

--- src/source/fs/mod.rs
+++ src/source/fs/mod.rs
@@ -18,8 +18,6 @@ use crate::{
 };
 
 mod event;
-#[cfg(test)]
-mod test;
 use self::event::FsEvent;
 
 pub struct FsLogSource {
@@ -112,4 +110,78 @@ impl LogSource for FsLogSource {
 
         Box::new(rx)
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::Stream;
+    use std::{
+        env,
+        fs::{self, File},
+    };
+
+    use crate::source::{LogRecord, LogSource};
+
+    use super::{FsLogSource, FsLogSourceConfig};
+
+    fn record(title: &str, body: &str) -> LogRecord {
+        LogRecord {
+            title: String::from(title),
+            body: String::from(body),
+        }
+    }
+
+    #[test]
+    fn main() {
+        let base_path = env::temp_dir().join("logram_test");
+        if base_path.exists() {
+            fs::remove_dir_all(&base_path).unwrap();
+        }
+        fs::create_dir(&base_path).unwrap();
+
+        let base_path_string = base_path.display().to_string();
+        let config = FsLogSourceConfig {
+            entries: vec![base_path_string],
+        };
+
+        let source = FsLogSource::new(config).unwrap();
+        let mut stream = source.into_stream().wait();
+
+        let file_path = base_path.join("file");
+        let new_file_path = base_path.join("file_renamed");
+
+        File::create(&file_path).unwrap();
+        assert_eq!(
+            stream.next().unwrap().unwrap(),
+            record("Filesystem", "/tmp/logram_test/file created")
+        );
+
+        fs::write(&file_path, b"content").unwrap();
+        assert_eq!(
+            stream.next().unwrap().unwrap(),
+            record("/tmp/logram_test/file", "content")
+        );
+
+        fs::rename(&file_path, &new_file_path).unwrap();
+        assert_eq!(
+            stream.next().unwrap().unwrap(),
+            record(
+                "Filesystem",
+                "/tmp/logram_test/file renamed to /tmp/logram_test/file_renamed",
+            )
+        );
+
+        fs::remove_file(&new_file_path).unwrap();
+        assert_eq!(
+            stream.next().unwrap().unwrap(),
+            record("Filesystem", "/tmp/logram_test/file_renamed removed")
+        );
+
+        fs::remove_dir(&base_path).unwrap();
+        assert_eq!(
+            stream.next().unwrap().unwrap(),
+            record("Filesystem", "/tmp/logram_test removed")
+        );
+    }
+
 }
--- src/source/journald/mod.rs
+++ src/source/journald/mod.rs
@@ -9,8 +9,6 @@ use crate::{
 };
 
 mod record;
-#[cfg(test)]
-mod test;
 use self::record::map_record;
 
 pub struct JournaldLogSource {
@@ -57,4 +55,58 @@ impl LogSource for JournaldLogSource {
 
         Box::new(rx)
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::Stream;
+    use systemd::journal;
+
+    use crate::{
+        config::JournaldLogSourceConfig,
+        source::{LogRecord, LogSource},
+    };
+
+    use super::JournaldLogSource;
+
+    fn send_log_record(keys: Vec<(&str, &str)>) {
+        let keys: Vec<String> = keys
+            .iter()
+            .map(|(key, value)| format!("{}={}", key, value))
+            .collect();
+
+        let keys = keys.iter().map(AsRef::as_ref).collect::<Vec<_>>();
+
+        journal::send(&keys);
+    }
+
+    fn record(title: &str, body: &str) -> LogRecord {
+        LogRecord {
+            title: String::from(title),
+            body: String::from(body),
+        }
+    }
+
+    #[test]
+    fn main() {
+        let config = JournaldLogSourceConfig {
+            units: vec![String::from("user@1000.service")],
+        };
+
+        let source = JournaldLogSource::new(config).unwrap();
+        let mut stream = source.into_stream().wait();
+
+        send_log_record(vec![("MESSAGE", "logram test")]);
+        assert_eq!(
+            stream.next().unwrap().unwrap(),
+            record("user@1000.service", "logram test")
+        );
+
+        send_log_record(vec![("MESSAGE", "logram test 2")]);
+        assert_eq!(
+            stream.next().unwrap().unwrap(),
+            record("user@1000.service", "logram test 2")
+        );
+    }
+
 }
